ed5dcd6565b8feb147e550201fe8150d9f366741
Tiengh <tienguyen231@gmail.com>
2024-01-03 15:11:53 +0700

    forget password api

--- src/main/java/group6/ecommerce/controller/UserController.java
+++ src/main/java/group6/ecommerce/controller/UserController.java
@@ -1,6 +1,8 @@
 package group6.ecommerce.controller;
 
 
+import group6.ecommerce.model.Users;
+import group6.ecommerce.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final EmailService emailService;
 
     @PostMapping("login")
     public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest) {
@@ -27,5 +30,16 @@ public class UserController {
     public ResponseEntity<String> register(@RequestBody UserRequest userRequest) {
         userService.register(userRequest);
         return ResponseEntity.status(HttpStatus.OK).body("Register successfully");
+    }
+
+    @PostMapping("/forgetPassWord/{email}")
+    public ResponseEntity<String> forgetPassWord(@PathVariable String email){
+        Users users = userService.findByEmail(email);
+        if(users != null){
+            emailService.SendEmailTo(email, users.getPassword());
+            return ResponseEntity.status(HttpStatus.OK).body("Password was sent to your email");
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong email");
+        }
     }
 }
---
+++ src/main/java/group6/ecommerce/service/impl/EmailServiceImpls.java
@@ -0,0 +1,22 @@
+package group6.ecommerce.service.impl;
+
+import group6.ecommerce.service.EmailService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpls implements EmailService {
+    public final JavaMailSender javaMailSender;
+    public EmailServiceImpls(JavaMailSender javaMailSender){
+        this.javaMailSender = javaMailSender;
+    }
+    @Override
+    public void SendEmailTo(String email, String node) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(email);
+        simpleMailMessage.setSubject("Forget PassWord");
+        simpleMailMessage.setText("Your password: " + node);
+        javaMailSender.send(simpleMailMessage);
+    }
+}

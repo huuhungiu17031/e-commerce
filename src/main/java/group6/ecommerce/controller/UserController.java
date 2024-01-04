b1e7f4f74ee7eea9bee5b9b454a99bb256adc4ea
Tiengh <tienguyen231@gmail.com>
2024-01-04 09:39:26 +0700

    forget password and change password api

--- src/main/java/group6/ecommerce/controller/UserController.java
+++ src/main/java/group6/ecommerce/controller/UserController.java
@@ -2,9 +2,12 @@ package group6.ecommerce.controller;
 
 
 import group6.ecommerce.model.Users;
+import group6.ecommerce.payload.request.ChangePasswordRequest;
 import group6.ecommerce.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import group6.ecommerce.payload.request.LoginRequest;
@@ -32,14 +35,25 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body("Register successfully");
     }
 
-    @PostMapping("/forgetPassWord/{email}")
-    public ResponseEntity<String> forgetPassWord(@PathVariable String email){
+    @PostMapping("forgetPassword/{email}")
+    public ResponseEntity<String> forgetPassword(@PathVariable String email){
         Users users = userService.findByEmail(email);
         if(users != null){
             emailService.SendEmailTo(email, users.getPassword());
             return ResponseEntity.status(HttpStatus.OK).body("Password was sent to your email");
         }else{
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong email");
+        }
+    }
+
+    @PostMapping("changePassword")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest) {
+        Boolean changePassword = userService.changePassword(changePasswordRequest);
+        if(changePassword){
+            return ResponseEntity.status(HttpStatus.OK).body("Change password successfully");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.OK).body("Wrong old password or the new password and confirm password do not match");
         }
     }
 }
--- src/main/java/group6/ecommerce/service/UserService.java
+++ src/main/java/group6/ecommerce/service/UserService.java
@@ -1,6 +1,7 @@
 package group6.ecommerce.service;
 
 import group6.ecommerce.model.Users;
+import group6.ecommerce.payload.request.ChangePasswordRequest;
 import group6.ecommerce.payload.request.LoginRequest;
 import group6.ecommerce.payload.request.UserRequest;
 import group6.ecommerce.payload.response.JwtResponse;
@@ -13,4 +14,5 @@ public interface UserService {
     Users findByEmail(String email);
 
     Users findById (int id);
+    Boolean changePassword(ChangePasswordRequest changePasswordRequest);
 }
